7a40f8a00a85f0ebdbaea9455fd77bda556b19f8
Andrii Kovalets <andriykovalec@gmail.com>
2022-09-02 18:32:48 +0300

    Add repository.

--- TestTask.Infrastructure/StartupSetup.cs
+++ TestTask.Infrastructure/StartupSetup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using TestTask.Core.Inrerfaces.Repisitories;
 using TestTask.Infrastructure.Data;
+using TestTask.Infrastructure.Data.Repositories;
 
 namespace TestTask.Infrastructure
 {
@@ -10,13 +12,19 @@ namespace TestTask.Infrastructure
         public static void SetupInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddTestTaskDbContext(configuration);
+            services.AddRepositories();
         }
 
-        public static void AddTestTaskDbContext(this IServiceCollection services, IConfiguration configuration)
+        private static void AddTestTaskDbContext(this IServiceCollection services, IConfiguration configuration)
         {
             services
                 .AddDbContext<TestTaskDbContext>(x =>
                     x.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+
+        private static void AddRepositories(this IServiceCollection services)
+        {
+            services.AddScoped(typeof(IRepository<>), typeof(BaseRepository<>));
         }
     }
 }
