0f8566896322758dce3c6a273e5c3bfcc15bd07e
Andrii Kovalets <andriykovalec@gmail.com>
2022-09-02 22:58:00 +0300

    Fix config DI in core project.

--- TestTask.Core/StartupSetup.cs
+++ TestTask.Core/StartupSetup.cs
@@ -12,9 +12,10 @@ namespace TestTask.Core
         public static void SetupCore(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddAutoMapper();
+            services.AddFluentValitation();
         }
 
-        public static void AddAutoMapper(this IServiceCollection services)
+        private static void AddAutoMapper(this IServiceCollection services)
         {
             var mapperConfig = new MapperConfiguration(mc =>
             {
@@ -25,7 +26,7 @@ namespace TestTask.Core
             services.AddSingleton(mapper);
         }
 
-        public static void AddFluentValitation(this IServiceCollection services)
+        private static void AddFluentValitation(this IServiceCollection services)
         {
             services
                 .AddFluentValidationAutoValidation()
