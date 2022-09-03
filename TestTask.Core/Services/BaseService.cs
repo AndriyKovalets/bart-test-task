14e9fdf0a5776629426327ef439e01431dc77421
Andrii Kovalets <andriykovalec@gmail.com>
2022-09-04 01:11:58 +0300

    Add base service and controller.

---
+++ TestTask.Core/Services/BaseService.cs
@@ -0,0 +1,68 @@
+﻿using AutoMapper;
+using TestTask.Core.Inrerfaces.Repisitories;
+using TestTask.Core.Inrerfaces.Services;
+using TestTask.Domain.Entities;
+using TestTask.Domain.Exceptions;
+
+namespace TestTask.Core.Services
+{
+    internal abstract class BaseService<TAddDto, TInfoDto, TUndateDto, TKey, TEntity>
+        : IBaseService<TAddDto, TInfoDto, TUndateDto, TKey>
+            where TEntity: class, IBaseEntity
+    {
+        private readonly IRepository<TEntity> _repository;
+        private readonly IMapper _mapper;
+
+        public BaseService(IRepository<TEntity> repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public virtual async Task<TInfoDto> AddAsync(TAddDto addDto)
+        {
+            var entityToAdd = _mapper.Map<TEntity>(addDto);
+
+            await _repository.AddAsync(entityToAdd);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<TInfoDto>(entityToAdd);
+        }
+
+        public virtual async Task<List<TInfoDto>> GetAllAsync()
+        {
+            var entityList = await _repository.GetAllAsync();
+
+            return _mapper.Map<List<TInfoDto>>(entityList);
+        }
+
+        public virtual async Task<TInfoDto> GetByKeyAsync(TKey key)
+        {
+            var entity = await _repository.GetByKeyAsync(key);
+
+            if (entity is null)
+            {
+                throw new NotFoundHttpException($"{typeof(TEntity).Name} with this key not found");
+            }
+
+            return _mapper.Map<TInfoDto>(entity);
+        }
+
+        public virtual async Task<TInfoDto> UpdateAsync(TKey key, TUndateDto undateDto)
+        {
+            var entity = await _repository.GetByKeyAsync(key);
+
+            if (entity is null)
+            {
+                throw new NotFoundHttpException($"{typeof(TEntity).Name} with this key not found");
+            }
+
+            _mapper.Map(undateDto, entity);
+
+            await _repository.UpdateAsync(entity);
+            await _repository.SaveChangesAsync();
+
+            return _mapper.Map<TInfoDto>(entity);
+        }
+    }
+}
---
+++ TestTask.WebApi/Controllers/BaseController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TestTask.Core.Inrerfaces.Services;
+using TestTask.Domain.Dtos.ContactDtos;
+
+namespace TestTask.WebApi.Controllers
+{
+    [ApiController]
+    public abstract class BaseController<TAddDto, TInfoDto, TUndateDto, TKey, TService> : ControllerBase
+        where TService: IBaseService<TAddDto, TInfoDto, TUndateDto, TKey>
+    {
+        private readonly TService _service;
+
+        public BaseController(TService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        public virtual async Task<IActionResult> AddAsync([FromBody] TAddDto addtDto)
+        {
+            var entity = await _service.AddAsync(addtDto);
+
+            return Ok(entity);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ContactInfoDto>>> GetAllAsync()
+        {
+            var entityList = await _service.GetAllAsync();
+
+            return Ok(entityList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ContactInfoDto>> GetAsync([FromRoute] TKey id)
+        {
+            var entity = await _service.GetByKeyAsync(id);
+
+            return Ok(entity);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<ContactInfoDto>> UpdateAsync(
+            [FromRoute] TKey id,
+            [FromBody] TUndateDto updateContactDto)
+        {
+            var entity = await _service.UpdateAsync(id, updateContactDto);
+
+            return Ok(entity);
+        }
+    }
+}
